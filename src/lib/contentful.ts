4928df2c48d1733ce1292da0d7af89f139ce9da0
Andy-git985 <andy.yu617@gmail.com>
2024-03-03 21:34:03 -0500

    Main project index and project [slug] page templates

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,16 +1,49 @@
 import contentful from 'contentful';
-import type { EntryFieldTypes } from 'contentful';
 
-export interface BlogPost {
-  contentTypeId: 'blogPost';
-  fields: {
-    title: EntryFieldTypes.Text;
-    content: EntryFieldTypes.RichText;
-    date: EntryFieldTypes.Date;
-    description: EntryFieldTypes.Text;
-    slug: EntryFieldTypes.Text;
-  };
+import type {
+  ChainModifiers,
+  Entry,
+  EntryFieldTypes,
+  EntrySkeletonType,
+  LocaleCode,
+} from 'contentful';
+
+export interface TypeBlogPostFields {
+  title?: EntryFieldTypes.Symbol;
+  date?: EntryFieldTypes.Date;
+  slug?: EntryFieldTypes.Symbol;
+  description?: EntryFieldTypes.Symbol;
+  content?: EntryFieldTypes.RichText;
+  images?: EntryFieldTypes.Array<EntryFieldTypes.AssetLink>;
+  type?: EntryFieldTypes.EntryLink<EntrySkeletonType>;
 }
+
+export type TypeBlogPostSkeleton = EntrySkeletonType<
+  TypeBlogPostFields,
+  'blogPost'
+>;
+
+export type TypeBlogPost<
+  Modifiers extends ChainModifiers,
+  Locales extends LocaleCode
+> = Entry<TypeBlogPostSkeleton, Modifiers, Locales>;
+
+export interface TypeProjectTypeFields {
+  title?: EntryFieldTypes.Symbol;
+  projects?: EntryFieldTypes.Array<
+    EntryFieldTypes.EntryLink<EntrySkeletonType>
+  >;
+}
+
+export type TypeProjectTypeSkeleton = EntrySkeletonType<
+  TypeProjectTypeFields,
+  'projectType'
+>;
+
+export type TypeProjectType<
+  Modifiers extends ChainModifiers,
+  Locales extends LocaleCode
+> = Entry<TypeProjectTypeSkeleton, Modifiers, Locales>;
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
